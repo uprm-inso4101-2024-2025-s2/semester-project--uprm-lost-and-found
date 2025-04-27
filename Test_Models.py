71831547baf761fa613e576e8bbab09605e3de88
Naedra <naedra.feliciano@upr.edu>
2025-04-27 12:24:41 -0400

    Claim items, Locations, Matches and Found items models and a tester file for the models

---
+++ Test_Models.py
@@ -0,0 +1,194 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models.models import Base, User, LostItem, Location, ClaimItems, FoundItems, Matches, Comments
+from datetime import date
+#Run by installing pytest
+from datetime import date
+import pytest
+import os
+
+# to test the files locally
+DATA_URL = "sqlite:///./test.db"
+engine = create_engine(DATA_URL, echo = True)
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    if os.path.exists("./test.db"):
+        os.remove("./test.db")
+    Base.metadata.create_all(bind=engine)
+    yield  # Allow tests to run
+    if os.path.exists("./test.db"):
+        os.remove("./test.db")
+
+#Base.metadata.create_all(bind=engine)
+@pytest.fixture
+def test_models():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def test_user(test_models):
+    db = test_models
+
+    new_user = User(name = "Paty", email = "test@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    assert new_user.name == "Paty"
+    assert new_user.email == "test@upr.edu"
+    assert new_user.password_hash == "NotaPassword"
+    assert new_user.profile_photo == b"imagebytes"
+    assert new_user.role == "user"
+    assert new_user.id is not None
+
+def  test_LostItem(test_models):
+    
+    db = test_models
+
+    new_user = User(name = "Paty", email = "tesit@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    lost = LostItem(description= "Lost Phone", published_date= date(2025, 4, 24), photo = b"photo", information = "Iphone 5 Lost in S 113", u_id =new_user.id)
+    db.add(lost)
+    db.commit()
+    db.refresh(lost)
+
+    assert lost.description == "Lost Phone"
+    assert lost.published_date == date(2025, 4, 24)
+    assert lost.photo == b"photo"
+    assert lost.information == "Iphone 5 Lost in S 113"
+    assert lost.u_id == new_user.id
+
+def test_FoundItems(test_models):
+    db = test_models
+
+    new_user = User(name = "Paty", email = "tesiit@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    location = Location(name = "S 113", coordinates= "18.2345, -59.8992")
+    db.add(location)
+    db.commit()
+    db.refresh(location)
+
+    found = FoundItems(photo = b"photo", description = "Found $20", published_date=date(2025, 4, 24), place_found="S- 113", additional_details= "It was on the last row of seats",u_id=new_user.id, p_id=location.id) 
+    db.add(found)
+    db.commit()
+    db.refresh(found)
+
+    assert found.photo == b"photo"
+    assert found.description == "Found $20"
+    assert found.published_date == date(2025, 4, 24)
+    assert found.place_found == "S- 113"
+    assert found.additional_details =="It was on the last row of seats"
+    assert found.u_id == new_user.id
+    assert found.p_id == location.id
+
+def test_location(test_models):
+    db = test_models
+
+    
+
+    location = Location(name = "S 113", coordinates= "18.2345, -59.8992")
+    db.add(location)
+    db.commit()
+    db.refresh(location)
+
+    assert location.name == "S 113"
+    assert location.coordinates ==  "18.2345, -59.8992"
+
+
+def test_ClaimItems(test_models):
+     db = test_models
+
+     new_user = User(name = "Paty", email = "testy@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+     db.add(new_user)
+     db.commit()
+     db.refresh(new_user)
+
+     location = Location(name = "S 113", coordinates= "18.2345, -59.8992")
+     db.add(location)
+     db.commit()
+     db.refresh(location)
+
+     found = FoundItems(photo = b"photo", description = "Found $20", published_date=date(2025, 4, 24), place_found="S- 113", additional_details= "It was on the last row of seats",u_id=new_user.id, p_id=location.id) 
+     db.add(found)
+     db.commit()
+     db.refresh(found)
+
+     claim = ClaimItems(f_id = found.id, u_id = new_user.id, status= "pending")
+     db.add(claim)
+     db.commit()
+     db.refresh(claim)
+
+     assert claim.status == "pending"
+     assert claim.f_id == found.id
+     assert claim.u_id == new_user.id
+
+def test_Comments(test_models):
+     db = test_models
+
+     new_user = User(name = "Paty", email = "trest@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+     db.add(new_user)
+     db.commit()
+     db.refresh(new_user)
+
+     lost = LostItem(description= "Lost Phone", published_date= date(2025, 4, 24), photo = b"photo", information = "Iphone 5 Lost in S 113", u_id =new_user.id)
+     db.add(lost)
+     db.commit()
+     db.refresh(lost)
+
+     comment = Comments(content= "I found a Iphone 5", publish_date=date(2025, 4, 24), u_id = new_user.id, l_id = lost.id)
+     db.add(comment)
+     db.commit()
+     db.refresh(comment)
+
+     assert comment.content ==  "I found a Iphone 5"
+     assert comment.publish_date == date(2025, 4, 24)
+     assert comment.u_id == new_user.id
+     assert comment.l_id == lost.id
+
+def test_matches(test_models):
+     db = test_models
+
+     new_user = User(name = "Paty", email = "ttest@upr.edu", password_hash = "NotaPassword", profile_photo =b"imagebytes", role = "user" )
+     db.add(new_user)
+     db.commit()
+     db.refresh(new_user)
+
+     lost = LostItem(description= "Lost Phone", published_date= date(2025, 4, 24), photo = b"photo", information = "Iphone 5 Lost in S 113", u_id =new_user.id)
+     db.add(lost)
+     db.commit()
+     db.refresh(lost)
+
+     location = Location(name = "S 113", coordinates= "18.2345, -59.8992")
+     db.add(location)
+     db.commit()
+     db.refresh(location)
+
+     found = FoundItems(photo = b"photo", description = "Found $20", published_date=date(2025, 4, 24), place_found="S- 113", additional_details= "It was on the last row of seats",u_id=new_user.id, p_id=location.id) 
+     db.add(found)
+     db.commit()
+     db.refresh(found)
+
+     import datetime
+
+     matches = Matches(l_id = lost.id, f_id = found.id, match_confidence_score= 0.80, status = "pending", date_matched=datetime.datetime(2025, 4, 24))
+     db.add(matches)
+     db.commit()
+     db.refresh(matches)
+
+     assert matches.l_id == lost.id
+     assert matches.f_id == found.id
+     assert matches.match_confidence_score == 0.80
+     assert matches.status == "pending"
+     assert matches.date_matched == datetime.datetime(2025, 4, 24)
+     #print("Lost item id: ", lost.id, " Found item id: ",  found.id, "Score: ", matches.match_confidence_score, "Status: ", matches.status, " Date: ", matches.date_matched)
+
--- database/models/models.py
+++ database/models/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Date,DateTime, ForeignKey,LargeBinary,Enum,DATE
 from sqlalchemy.orm import DeclarativeBase
+from datetime import date
+
+
 
 from typing import List
 from typing import Optional
@@ -14,33 +17,75 @@ class Base(DeclarativeBase):
     pass
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Users" 
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    password_hash = Column(String(255), nullable=False)
+    id = Column("U_ID",Integer, primary_key=True, index=True)
+    name = Column("U_Username",String(100), nullable=False)
+    email = Column("U_Email",String(100), unique=True, nullable=False)
+    password_hash = Column("U_Password",String(500), nullable=False)
+    #adding missing column according  to Tables : Profile photo
+    profile_photo = Column("U_ProfilePhoto",LargeBinary, nullable= False)
     role = Column(String(20), default="user")  # 'admin' or 'user'
 
 class Matches(Base):
-    __tablename__ = "matches"
+    __tablename__ = "Matches"
 
-    m_id = Column(Integer, primary_key=True, index=True)
-    l_id = Column(Integer, ForeignKey('lostitems.l_id'), nullable=False)
-    f_id = Column(Integer, ForeignKey('founditems.f_id'), nullable=False)
-    match_confidence_score = Column(Float, nullable=False)
-    date_matched = Column(Date, nullable=False)
-    status = Column(String(20), nullable=False)
+    m_id = Column("M_ID",Integer, primary_key=True, index=True)
+    l_id = Column(Integer, ForeignKey('LostItems.L_ID'), nullable=False)
+    f_id = Column(Integer, ForeignKey('FoundItems.F_ID'), nullable=False)
+    match_confidence_score = Column("M_MatchScore",Float, nullable=False)
+    date_matched = Column("M_DateMatched",DateTime, nullable=False)
+    status = Column("M_Status",String(20), nullable=False)
 
-import datetime;
+import datetime
 
 class LostItem(Base):
     __tablename__="LostItems"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    description: Mapped[str] = mapped_column(String(120))
+    id: Mapped[int] = mapped_column("L_ID",primary_key=True)
+    description: Mapped[str] = mapped_column("L_Description",String(120), nullable = False)
 
     # change string to a date Object
-    published_date:Mapped[str]= mapped_column(String())
-    photo: Mapped[str] = mapped_column(String())
-    information: Mapped[str] = mapped_column(String(500))
+    published_date:Mapped[date]= mapped_column("L_PublishDate",Date, nullable = False)
+    photo: Mapped[bytes] = mapped_column("L_Photo",LargeBinary)
+    information: Mapped[str] = mapped_column("L_Information",String(500))
+    #adding missing u_id from the LostItems Table.txt
+    u_id =  mapped_column(ForeignKey("Users.U_ID"))
+
+class FoundItems(Base):
+    __tablename__ = "FoundItems"
+    id = Column("F_ID", Integer,primary_key = True, autoincrement = True  )
+    photo= Column("F_Photo", LargeBinary, nullable= True )
+    description = Column("F_Description",String(120), nullable = False )
+    published_date = Column("F_PublishDate", Date, nullable= False)
+    place_found = Column("F_PlaceFound",String(20), nullable= True)
+    additional_details = Column("F_AdditionalDetails", String(500), nullable=True )
+    u_id = Column(Integer, ForeignKey("Users.U_ID", ondelete="CASCADE") , nullable=False)
+    p_id = Column(Integer, ForeignKey("Location.P_ID", ondelete= "CASCADE"),nullable = False)
+
+class ClaimItems(Base):
+    __tablename__= "ClaimItems"
+    id = Column("CR_ID", Integer,primary_key= True, autoincrement=True )
+    f_id = Column(Integer, ForeignKey("FoundItems.F_ID",ondelete="CASCADE"), nullable= False)
+    u_id = Column(Integer, ForeignKey("Users.U_ID", ondelete="CASCADE"), nullable = False)
+    status = Column(Enum('pending', 'claimed', 'declined'), default='pending')
+
+
+class Location(Base):
+    __tablename__ = "Location"
+    id = Column("P_ID", Integer, primary_key= True, autoincrement= True)
+    name = Column("P_Name", String(20), nullable= False)
+    coordinates = Column("P_Coordinates", String(30), nullable= False)
+
+class Comments(Base):
+    __tablename__ = "Comments"
+    id = Column("C_ID", Integer, primary_key= True, autoincrement=True)
+    content = Column("C_Content", String(500), nullable= False)
+    publish_date = Column("C_PublishDate",Date , nullable= False)
+    u_id= Column(Integer, ForeignKey("Users.U_ID", ondelete="CASCADE"), nullable = False)
+    l_id= Column(Integer, ForeignKey("LostItems.L_ID", ondelete="CASCADE"), nullable = False)
+
+
+
+
+
     
